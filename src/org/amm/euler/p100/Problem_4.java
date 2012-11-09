41a825fc40c6f2268422415023da0945cb23c20e
amm <andriy.amm@gmail.com>
2012-11-09 12:05:24 +0200

    added biginteger solutition to roblem 3

--- src/org/amm/euler/p100/Problem_4.java
+++ src/org/amm/euler/p100/Problem_4.java
@@ -1,7 +1,13 @@
 package org.amm.euler.p100;
 
 public class Problem_4 {
+	
 	public static Long solution_1(){
 		return null;
+	}
+	
+	public boolean  isPalindrome(Long n){
+		
+		return true;
 	}
 }
