05e296ae819fe20e77c11072be9507c44d5a2384
amm <andriy.amm@gmail.com>
2012-11-27 10:57:02 +0200

    working on 11 problem

--- src/org/amm/euler/utils/EulerUtils.java
+++ src/org/amm/euler/utils/EulerUtils.java
@@ -7,19 +7,54 @@ import java.util.Scanner;
 
 public class EulerUtils {
 
-	public static ArrayList<ArrayList<Integer>> read2darray(String filename)
+	/**
+	 * read matrix from text file
+	 * 
+	 * @param filename
+	 * 			name of file  
+	 * @return matrix in as ArrayList of <ArrayList<Integer>
+	 * @throws FileNotFoundException 
+	 */
+	public static ArrayList<ArrayList<Integer>> readMatrix(String filename)
 			throws FileNotFoundException {
 
-		ArrayList<ArrayList<Integer>> a = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
 		Scanner input = new Scanner(new File(filename));
 		while (input.hasNextLine()) {
-			Scanner colReader = new Scanner(input.nextLine());
-			ArrayList<Integer> col = new ArrayList<Integer>();
-			while (colReader.hasNextInt()) {
-				col.add(colReader.nextInt());
+			Scanner rowReader = new Scanner(input.nextLine());
+			ArrayList<Integer> row = new ArrayList<Integer>();
+			while (rowReader.hasNextInt()) {
+				row.add(rowReader.nextInt());
 			}
-			a.add(col);
+			matrix.add(row);
 		}
-		return a;
+		return matrix;
+	}
+	
+	/**
+	 * read matrix from text file
+	 * 
+	 * @param filename
+	 * 			name of file  
+	 * @return two dimension array
+	 * @throws FileNotFoundException 
+	 */
+	public static int[][] readMatrix(String filename, Integer row, Integer col)
+			throws FileNotFoundException {
+
+		int[][] matrix = new int[row][col];
+		int j, i = 0;
+		Scanner input = new Scanner(new File(filename));
+		
+		while (input.hasNextLine()) {
+			Scanner rowReader = new Scanner(input.nextLine());
+			j = 0;
+			while (rowReader.hasNextInt()) {
+				matrix[i][j] = rowReader.nextInt();
+				j++;
+			}
+			i++;
+		}
+		return matrix;
 	}
 }
--- test/org/amm/euler/p100/Problem_11_Test.java
+++ test/org/amm/euler/p100/Problem_11_Test.java
@@ -8,11 +8,21 @@ public class Problem_11_Test {
 	
 	@Test
 	public void solution_0_test() {
-		assertEquals(17, Problem_11.solution_0());
+	//	assertEquals(17, Problem_11.solution_0(20, 20));
 	}
 	
 	@Test
 	public void solution_1_test() {
-		assertEquals(17, Problem_11.solution_1());
+		assertEquals(17, Problem_11.solution_1(20, 20));
+	}
+	
+	@Test
+	public void solution_2_test() {
+		assertEquals(17, Problem_11.solution_2(20, 20));
+	}
+	
+	@Test
+	public void solution_3_test() {
+		assertEquals(17, Problem_11.solution_3(20, 20));
 	}
 }
