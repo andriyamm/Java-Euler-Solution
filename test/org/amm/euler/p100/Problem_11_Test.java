ba16326ce3b206752bbb56c611dce0a0e524fb09
amm <andriy.amm@gmail.com>
2012-11-28 18:41:38 +0200

    fix problem 11

--- test/org/amm/euler/p100/Problem_11_Test.java
+++ test/org/amm/euler/p100/Problem_11_Test.java
@@ -2,27 +2,20 @@ package org.amm.euler.p100;
 
 import static org.junit.Assert.assertEquals;
 
+import java.math.BigInteger;
+
 import org.junit.Test;
 
 public class Problem_11_Test {
 	
 	@Test
 	public void solution_0_test() {
-	//	assertEquals(17, Problem_11.solution_0(20, 20));
+		//assertEquals(17, Problem_11.solution_0("problem_11.txt", 20, 20, 4));
 	}
 	
 	@Test
 	public void solution_1_test() {
-		assertEquals(17, Problem_11.solution_1(20, 20));
+		System.out.println(new BigInteger("2").pow(22138));
 	}
 	
-	@Test
-	public void solution_2_test() {
-		assertEquals(17, Problem_11.solution_2(20, 20));
-	}
-	
-	@Test
-	public void solution_3_test() {
-		assertEquals(17, Problem_11.solution_3(20, 20));
-	}
 }
